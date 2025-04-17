4b09314e6a35299ca483282fd851174a53eebedf
debanshu-majumdar18 <debanshu.majumdar@m360research.com>
2025-04-17 13:54:46 +0530

    [PASSED-TEST] - added logic for custom delimiter format

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,14 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers = numbers.gsub("\n", ",")
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..-1]
+    end
+
+    numbers = numbers.gsub(/#{delimiter}|\n/, ",")
     numbers.split(',').map(&:to_i).sum
   end
 end
