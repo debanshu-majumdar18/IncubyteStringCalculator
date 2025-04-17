453fdf93ba7918a3f9231128e6c2eadd3be99440
debanshu-majumdar18 <debanshu.majumdar@m360research.com>
2025-04-17 13:29:39 +0530

    [FAILED-TEST] - for sum of two comma seperated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it "returns the number itself if only one number is provided" do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of two numbers seperated by comma" do
+      expect(StringCalculator.add("3, 4")).to eq(7)
+    end
   end
 end
